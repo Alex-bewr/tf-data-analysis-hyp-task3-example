d2ca3a69255f3e467822e728c6603c9d21c69df8
Alex-bewr <alexfilippov20000417@yandex.ru>
2023-04-13 17:37:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,17 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 441809625 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    alpha = 0.03
+    delta = y.mean() - x.mean()
+    
+    pvalue = ttest_ind(x, y, equal_var=False).pvalue
+    
+    if (pvalue < 2 * alpha and delta < 0):
+        buf = True
+    else: 
+        buf = False
+        
+    return buf
